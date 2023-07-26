49e10375750708038f3ad71deab94c9d2abc25cb
Frayu <galiano@outlook.com>
2023-07-26 14:43:15 -0500

    Design first card mobile

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,70 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div id="main-container">
+      <div id="primer-contenedor">
+        <div id="contenedor-usuario">
+          <Image src="/image-jeremy.png" width={70} height={70} alt="Foto Jeremy Robson" id="img-usuario" />
+          <div id="usuario-datos">
+            <p>Report for</p>
+            <p id="usuario-nombre">Jeremy Robson</p>
+          </div>          
+        </div>
+        <div id="contenedor-periodos">
+          <a>Dayly</a>
+          <a>Weekly</a>
+          <a>Monthly</a>
+        </div>          
+      </div>
+      <div id="segundo-contenedor">
+        <div className="card">
+          <Image src="/icon-work.svg" width={70} height={70} alt="icon-work" className="icons" />
+          <div className="botton-card">
+            <div className="card-title">
+              <p>Work</p>
+              <Image src="/icon-ellipsis.svg" width={20} height={5} alt="icon-ellipsis" className="icon-puntos" />
+            </div>
+            <div className="card-data">
+              <p className="horas">32hrs</p>
+              <p className="previous-data">
+                Last Week - 36 hrs
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <Image src="/icon-work.svg" width={70} height={70} alt="icon-work" className="icons" />
+          <div className="botton-card">
+            <div className="card-title">
+              <p>Work</p>
+              <Image src="/icon-ellipsis.svg" width={20} height={5} alt="icon-ellipsis" className="icon-puntos" />
+            </div>
+            <div className="card-data">
+              <p className="horas">32hrs</p>
+              <p className="previous-data">
+                Last Week - 36 hrs
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          
+          <Image src="/icon-work.svg" width={70} height={70} alt="icon-work" className="icons" />
+          <div className="botton-card">
+            <div className="card-title">
+              <p>Work</p>
+              <Image src="/icon-ellipsis.svg" width={20} height={5} alt="icon-ellipsis" className="icon-puntos" />
+            </div>
+            <div className="card-data">
+              <p className="horas">32hrs</p>
+              <p className="previous-data">
+                Last Week - 36 hrs
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
